3e01fa680c1b79e81de7b82c772ae36bfe5e3b1c
Frederik Blais <frederik.blais22@gmail.com>
2021-10-03 17:49:17 -0400

    Front end added

--- bot/twttrbot/views.py
+++ bot/twttrbot/views.py
@@ -1,3 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class Index(View):
+    template = 'index.html'
+
+    def get(self, request):
+        return render(request, self.template)
