f9d08aaf4c5950e88d42f6ff8afce7dceb30629e
Frederik Blais <frederik.blais22@gmail.com>
2021-10-03 13:03:32 -0400

    fixed linter errors

--- bot/twttrbot/admin.py
+++ bot/twttrbot/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import *
+from .models import TweetLookUpWord, TweetLookUpBadWord, OutboundDirectMessage, TweetLookUpCoordinates
 
 admin.site.register(TweetLookUpWord)
 admin.site.register(TweetLookUpBadWord)
